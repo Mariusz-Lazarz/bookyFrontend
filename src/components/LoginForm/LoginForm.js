3d8ae347302a5d632a0211f0ec8b348673f00014
Mariusz Łazarz <mariusz.lazarz97@gmail.com>
2023-06-26 16:24:33 +0200

    creating multiple input forms

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -9,21 +9,71 @@ const LoginForm = () => {
     valueInputIsInvalid: nameInputIsInvalid,
     valueOnChange: nameOnChange,
     valueOnBlur: nameOnBlur,
-    valueReset,
+    valueReset: nameReset,
   } = useInput((enteredName) => enteredName.trim() !== "");
 
+  const {
+    enteredValue: enteredEmail,
+    enteredValueIsValid: enteredEmailIsValid,
+    valueInputIsInvalid: emailInputIsInvalid,
+    valueOnChange: emailOnChange,
+    valueOnBlur: emailOnBlur,
+    valueReset: emailReset,
+  } = useInput((enteredEmail) => enteredEmail.trim().includes("@"));
+
+  const {
+    enteredValue: enteredPassword,
+    enteredValueIsValid: enteredPasswordIsValid,
+    valueInputIsInvalid: passwordInputIsInvalid,
+    valueOnChange: passwordOnChange,
+    valueOnBlur: passwordOnBlur,
+    valueReset: passwordReset,
+  } = useInput((enteredPassword) => enteredPassword.trim().length > 6);
+
+  const {
+    enteredValue: enteredConfirmPassword,
+    enteredValueIsValid: enteredConfirmPasswordIsValid,
+    valueInputIsInvalid: confirmPasswordInputIsInvalid,
+    valueOnChange: confirmPasswordOnChange,
+    valueOnBlur: confirmPasswordOnBlur,
+    valueReset: confirmPasswordReset,
+  } = useInput(
+    (enteredConfirmPassword) =>
+      enteredConfirmPassword.trim() === enteredPassword.trim()
+  );
+
   let formIsValid = false;
-  if (enteredNameIsValid) {
+  if (
+    enteredNameIsValid &&
+    enteredPasswordIsValid &&
+    enteredEmailIsValid &&
+    enteredConfirmPasswordIsValid
+  ) {
     formIsValid = true;
   }
 
   const submitHandler = (e) => {
     e.preventDefault();
-    if (!enteredNameIsValid) return;
-    valueReset();
+    if (!enteredNameIsValid || !enteredEmailIsValid || !enteredPasswordIsValid)
+      return;
+    nameReset();
+    passwordReset();
+    emailReset();
+    confirmPasswordReset();
   };
 
   const nameInputClasses = nameInputIsInvalid
+    ? " border-red-700 border-2"
+    : "border-2";
+
+  const passwordInputClasses = passwordInputIsInvalid
+    ? " border-red-700 border-2"
+    : "border-2";
+
+  const emailInputClasses = emailInputIsInvalid
+    ? " border-red-700 border-2"
+    : "border-2";
+  const confirmPasswordInputClasses = confirmPasswordInputIsInvalid
     ? " border-red-700 border-2"
     : "border-2";
   return (
@@ -39,7 +89,7 @@ const LoginForm = () => {
           <div></div>
         </div>
         <div className="p-2">
-          <h1 className="text-lg font-medium mb-2">Hello to booky</h1>
+          <h1 className="text-lg font-medium mb-2">Welcome to booky</h1>
           <form className=" flex flex-col gap-2" onSubmit={submitHandler}>
             <div>
               <input
@@ -58,11 +108,59 @@ const LoginForm = () => {
               )}
             </div>
             <div>
+              <input
+                type="email"
+                id="email"
+                placeholder="Your email..."
+                value={enteredEmail}
+                onChange={emailOnChange}
+                onBlur={emailOnBlur}
+                className={`${emailInputClasses} rounded-md p-1 w-full outline-none`}
+              ></input>
+              {emailInputIsInvalid && (
+                <p className="text-red-700 font-semibold">
+                  Enter a valid email!
+                </p>
+              )}
+            </div>
+            <div>
+              <input
+                type="password"
+                id="password"
+                placeholder="Your password..."
+                value={enteredPassword}
+                onChange={passwordOnChange}
+                onBlur={passwordOnBlur}
+                className={`${passwordInputClasses} rounded-md p-1 w-full outline-none`}
+              ></input>
+              {passwordInputIsInvalid && (
+                <p className="text-red-700 font-semibold">
+                  Password has to be longer than 6 chars!
+                </p>
+              )}
+            </div>
+            <div>
+              <input
+                type="password"
+                id="confirmPassword"
+                placeholder="Confirm password..."
+                value={enteredConfirmPassword}
+                onChange={confirmPasswordOnChange}
+                onBlur={confirmPasswordOnBlur}
+                className={`${confirmPasswordInputClasses} rounded-md p-1 w-full outline-none`}
+              ></input>
+              {confirmPasswordInputIsInvalid && (
+                <p className="text-red-700 font-semibold">
+                  Password does not match
+                </p>
+              )}
+            </div>
+            <div>
               <button
                 type="submit"
                 disabled={!formIsValid}
                 className={`rounded-full p-2 w-full ${
-                  !formIsValid ? "bg-slate-500" : "bg-rose-500"
+                  !formIsValid ? "bg-gray-400" : "bg-rose-500"
                 }`}
               >
                 Submit
